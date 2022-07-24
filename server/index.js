73cf4cc43b13a96c261d5bc456458407a0c5dea5
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2022-07-24 15:20:28 +0200

    Connect socekt.io

--- server/index.js
+++ server/index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const cors = require('cors')
-const io = require('socket.io')
+const { Server } = require('socket.io')
 
 app.use(cors())
 
 const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors:{
+        origin: 'http://localhost:3000',
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`User joined: ${socket.id}`);
+})
 
 server.listen(3001, () => {
     console.log("server running");
